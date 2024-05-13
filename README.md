729c4c4dc07278bb334ebb3f4e1d0578f5b31a05
Michael Stanczyk <stanczyk4@gmail.com>
2024-05-13 17:47:18 -0500

    port to zephyr project and nrf connect sdk instead of nrf5

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # VibrationMonitor
+
+## Installation
+
+Install Zephyr and Zephyr SDK and set the environment variable `ZEPHYR_BASE` to the path of the Zephyr SDK.
+
+[zephyr](https://docs.zephyrproject.org/latest/develop/getting_started/index.html)
+
+Note: make sure to run until post install of the SDK. [zephyr sdk install](https://docs.zephyrproject.org/latest/develop/getting_started/index.html#install-the-zephyr-sdk)
+
+Install Nordic SDK and follow setup
+
+[nordic sdk](https://academy.nordicsemi.com/courses/nrf-connect-sdk-fundamentals/lessons/lesson-1-nrf-connect-sdk-introduction/)
+
+Note, after installing make sure to close all instances of IDE and re-open to get new environment variables.
+
+!Important: set the environment variable ZEPHYR_BASE to %HOMEPATH%\zephyrproject\zephyr or the path to the zephyr SDK, NOT the zephyrproject.
--- cmake/toolchains/gcc/gcc_arm_toolchain.cmake
+++ cmake/toolchains/gcc/gcc_arm_toolchain.cmake
@@ -10,40 +10,13 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 option(GCC_TOOLCHAIN_PATH "Location of GCC toolchain" "")
 option(TOOLCHAIN_ENABLE_LTO "Use Linker Time Optimization globally" OFF)
-option(TOOLCHAIN_DISABLE_RTTI "Enable RTTI" OFF)
-option(TOOLCHAIN_DISABLE_EXCEPTIONS "Enable exceptions" OFF)
-option(ENABLE_ALL_WARNINGS "Enable all warnings" OFF)
 
 if(NOT "${GCC_TOOLCHAIN_PATH}" STREQUAL "")
   set(ENV{GCC_TOOLCHAIN_PATH} "${GCC_TOOLCHAIN_PATH}")
 endif()
 
-# Workaround for CMake being weird with toolchain files and cache variables
-if (TOOLCHAIN_OPTIMIZATION_FLAG)
-  # Environment variables are always preserved.
-  set(ENV{_TOOLCHAIN_OPTIMIZATION_FLAG} "${TOOLCHAIN_OPTIMIZATION_FLAG}")
-else()
-  set(TOOLCHAIN_OPTIMIZATION_FLAG "$ENV{_TOOLCHAIN_OPTIMIZATION_FLAG}")
-endif()
-
-if(NOT TOOLCHAIN_OPTIMIZATION_FLAG)
-  message(FATAL_ERROR "Need to define optimization flag TOOLCHAIN_OPTIMIZATION_FLAG")
-endif()
-
 if(${TOOLCHAIN_ENABLE_LTO})
   set(ENV{_TOOLCHAIN_ENABLE_LTO} "${TOOLCHAIN_ENABLE_LTO}")
-endif()
-
-if(${TOOLCHAIN_DISABLE_RTTI})
-  set(ENV{_TOOLCHAIN_DISABLE_RTTI} "${TOOLCHAIN_DISABLE_RTTI}")
-endif()
-
-if(${TOOLCHAIN_DISABLE_EXCEPTIONS})
-  set(ENV{_TOOLCHAIN_DISABLE_EXCEPTIONS} "${TOOLCHAIN_DISABLE_EXCEPTIONS}")
-endif()
-
-if(${ENABLE_ALL_WARNINGS})
-  set(ENV{_ENABLE_ALL_WARNINGS} "${ENABLE_ALL_WARNINGS}")
 endif()
 
 # Find GCC for ARM
@@ -81,126 +54,108 @@ if(DEFINED ENV{_TOOLCHAIN_ENABLE_LTO})
   set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
 endif()
 
-if(DEFINED ENV{_TOOLCHAIN_OPTIMIZATION_FLAG})
-  add_compile_options(
-    "$<$<COMPILE_LANGUAGE:C,CXX>:$ENV{_TOOLCHAIN_OPTIMIZATION_FLAG}>"
-  )
-endif()
-
 # -m = ARM GCC Flags https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
 # -D preprocessor #define
-# -f = GCC Flags 
+# -f = GCC Flags
 # https://gcc.gnu.org/onlinedocs/gcc/Option-Summary.html
 # https://gcc.gnu.org/onlinedocs/gcc/Code-Gen-Options.html
 # https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html#Optimize-Options
 # https://gcc.gnu.org/onlinedocs/gcc/C-Dialect-Options.html#C-Dialect-Options
 # https://gcc.gnu.org/onlinedocs/gcc/Preprocessor-Options.html
-set(GCC_ASM_FLAGS ${GCC_ASM_FLAGS} 
-  "-D__STARTUP_CLEAR_BSS"
-  "-mapcs" # Synonym for -mapcs-frame, generate stack frame that is compliant with the ARM procedure call standard for all functions
-  "-fno-common" # Default setting, specifies compiler placement for uninitialized global variables to be in BSS
-  "-ffunction-sections" # Place each function item into its own section in the output file, this helps the linker optimize code
-  "-ffreestanding" # Asserts that the target is a freestanding environment, ie one which the standard lib may not exist
-  "-fno-builtin" # Do not recognize built in function that do not begin with __buildin_ as prefix
-)
+# set(GCC_ASM_FLAGS ${GCC_ASM_FLAGS}
+#   "-D__STARTUP_CLEAR_BSS"
+#   "-mapcs" # Synonym for -mapcs-frame, generate stack frame that is compliant with the ARM procedure call standard for all functions
+#   "-fno-common" # Default setting, specifies compiler placement for uninitialized global variables to be in BSS
+#   "-ffunction-sections" # Place each function item into its own section in the output file, this helps the linker optimize code
+#   "-ffreestanding" # Asserts that the target is a freestanding environment, ie one which the standard lib may not exist
+#   "-fno-builtin" # Do not recognize built in function that do not begin with __buildin_ as prefix
+# )
 
-set(GCC_C_FLAGS ${GCC_C_FLAGS}
-  "-DPRINTF_FLOAT_ENABLE=0"
-  "-DSCANF_FLOAT_ENABLE=0"
-  "-DPRINTF_ADVANCED_ENABLE=0"
-  "-DSCANF_ADVANCED_ENABLE=0"
-)
+# set(GCC_C_FLAGS ${GCC_C_FLAGS}
+#   "-DPRINTF_FLOAT_ENABLE=0"
+#   "-DSCANF_FLOAT_ENABLE=0"
+#   "-DPRINTF_ADVANCED_ENABLE=0"
+#   "-DSCANF_ADVANCED_ENABLE=0"
+# )
 
-set(GCC_CXX_FLAGS ${GCC_CXX_FLAGS} 
-  "-Wno-register"
-  "-fno-threadsafe-statics" # reduce flash by making statics/singleton not thread safe, since we arent using a threaded system
-)
+# set(GCC_CXX_FLAGS ${GCC_CXX_FLAGS}
+#   "-Wno-register"
+#   "-fno-threadsafe-statics" # reduce flash by making statics/singleton not thread safe, since we arent using a threaded system
+# )
 
-set(GCC_COMMON_C_CXX_FLAGS 
-  # "--specs=nosys.specs"
-  "--specs=nano.specs"
-  "-mapcs" # Synonym for -mapcs-frame, generate stack frame that is compliant with the ARM procedure call standard for all functions
-  "-MMD" 
-  "-MP" # This option instructs CPP to add a phony target for each dependency other than the main file
-  "-fno-common" # Default setting, specifies compiler placement for uninitialized global variables to be in BSS
-  "-ffunction-sections" # Place each function item into its own section in the output file, this helps the linker optimize code
-  "-fdata-sections" # Place each data item into its own section in the output file, this helps the linker optimize code
-  "-ffreestanding" # Asserts that the target is a freestanding environment, ie one which the standard lib may not exist
-  "-fno-builtin" # Do not recognize built in function that do not begin with __buildin_ as prefix
-)
+# set(GCC_COMMON_C_CXX_FLAGS
+#   # "--specs=nosys.specs"
+#   "--specs=nano.specs"
+#   "-mapcs" # Synonym for -mapcs-frame, generate stack frame that is compliant with the ARM procedure call standard for all functions
+#   "-MMD"
+#   "-MP" # This option instructs CPP to add a phony target for each dependency other than the main file
+#   "-fno-common" # Default setting, specifies compiler placement for uninitialized global variables to be in BSS
+#   "-ffunction-sections" # Place each function item into its own section in the output file, this helps the linker optimize code
+#   "-fdata-sections" # Place each data item into its own section in the output file, this helps the linker optimize code
+#   "-ffreestanding" # Asserts that the target is a freestanding environment, ie one which the standard lib may not exist
+#   "-fno-builtin" # Do not recognize built in function that do not begin with __buildin_ as prefix
+# )
 
-list(APPEND GCC_C_FLAGS ${GCC_COMMON_C_CXX_FLAGS})
-list(APPEND GCC_CXX_FLAGS ${GCC_COMMON_C_CXX_FLAGS})
+# list(APPEND GCC_C_FLAGS ${GCC_COMMON_C_CXX_FLAGS})
+# list(APPEND GCC_CXX_FLAGS ${GCC_COMMON_C_CXX_FLAGS})
 
-# Debug only flags for ASM
-set(GCC_ASM_DEBUG_FLAGS ${GCC_ASM_DEBUG_FLAGS}
-  "-g"
-)
+# # Debug only flags for ASM
+# set(GCC_ASM_DEBUG_FLAGS ${GCC_ASM_DEBUG_FLAGS}
+#   "-g"
+# )
 
-# Debug only flags for C and CXX
-set(GCC_COMMON_C_CXX_DEBUG_FLAGS ${GCC_COMMON_C_CXX_DEBUG_FLAGS}
-  "-g"
-)
+# # Debug only flags for C and CXX
+# set(GCC_COMMON_C_CXX_DEBUG_FLAGS ${GCC_COMMON_C_CXX_DEBUG_FLAGS}
+#   "-g"
+# )
 
-# Release only flags for ASM
-set(GCC_ASM_RELEASE_FLAGS ${GCC_ASM_RELEASE_FLAGS}
-)
+# # Release only flags for ASM
+# set(GCC_ASM_RELEASE_FLAGS ${GCC_ASM_RELEASE_FLAGS}
+# )
 
-# Release only flags for C and CXX
-set(GCC_COMMON_C_CXX_RELEASE_FLAGS ${GCC_COMMON_C_CXX_RELEASE_FLAGS}
-)
+# # Release only flags for C and CXX
+# set(GCC_COMMON_C_CXX_RELEASE_FLAGS ${GCC_COMMON_C_CXX_RELEASE_FLAGS}
+# )
 
-if(DEFINED ENV{_TOOLCHAIN_DISABLE_RTTI})
-  list(APPEND GCC_CXX_FLAGS "-fno-exceptions")
-endif()
+# add_compile_definitions(
+#   "$<$<CONFIG:DEBUG>:DEBUG>"
+#   "$<$<CONFIG:RELEASE>:NDEBUG>"
+# )
 
-if(DEFINED ENV{_TOOLCHAIN_DISABLE_EXCEPTIONS})
-  list(APPEND GCC_CXX_FLAGS "-fno-rtti")
-endif()
+# add_compile_options(
+#   "$<$<COMPILE_LANGUAGE:ASM>:${GCC_ASM_FLAGS}>" # Add ASM flags
+#   "$<$<COMPILE_LANGUAGE:C>:${GCC_C_FLAGS}>" # Add C flags
+#   "$<$<COMPILE_LANGUAGE:CXX>:${GCC_CXX_FLAGS}>" # Add CXX flags
+#   "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANGUAGE:ASM>>:${GCC_ASM_DEBUG_FLAGS}>" # Add ASM debug flags
+#   "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANGUAGE:C,CXX>>:${GCC_COMMON_C_CXX_DEBUG_FLAGS}>" # Add common C/CXX debug flags
+#   "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANGUAGE:ASM>>:${GCC_ASM_RELEASE_FLAGS}>" # Add ASM release flags
+#   "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANGUAGE:C,CXX>>:${GCC_COMMON_C_CXX_RELEASE_FLAGS}>" # Add common C/CXX release flags
+# )
 
-if(DEFINED ENV{_ENABLE_ALL_WARNINGS})
-  list(APPEND GCC_CXX_FLAGS "-Wall")
-endif()
-
-add_compile_definitions(
-  "$<$<CONFIG:DEBUG>:DEBUG>"
-  "$<$<CONFIG:RELEASE>:NDEBUG>"
-)
-
-add_compile_options(
-  "$<$<COMPILE_LANGUAGE:ASM>:${GCC_ASM_FLAGS}>" # Add ASM flags
-  "$<$<COMPILE_LANGUAGE:C>:${GCC_C_FLAGS}>" # Add C flags
-  "$<$<COMPILE_LANGUAGE:CXX>:${GCC_CXX_FLAGS}>" # Add CXX flags
-  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANGUAGE:ASM>>:${GCC_ASM_DEBUG_FLAGS}>" # Add ASM debug flags
-  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANGUAGE:C,CXX>>:${GCC_COMMON_C_CXX_DEBUG_FLAGS}>" # Add common C/CXX debug flags
-  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANGUAGE:ASM>>:${GCC_ASM_RELEASE_FLAGS}>" # Add ASM release flags
-  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANGUAGE:C,CXX>>:${GCC_COMMON_C_CXX_RELEASE_FLAGS}>" # Add common C/CXX release flags
-)
-
-# GCC Link options https://gcc.gnu.org/onlinedocs/gcc/Link-Options.html
-# LINKER inserts a -Wl,option where needed
-# Spec files, --specs:  https://gcc.gnu.org/onlinedocs/gcc/Spec-Files.html
+# # GCC Link options https://gcc.gnu.org/onlinedocs/gcc/Link-Options.html
+# # LINKER inserts a -Wl,option where needed
+# # Spec files, --specs:  https://gcc.gnu.org/onlinedocs/gcc/Spec-Files.html
 add_link_options(
-  "--specs=nano.specs"
-  # "--specs=nosys.specs"
-  # "LINKER:--start-group"
-  "-lc" # link with libc.a, the standard c lib
-  "-lm" # link with libm.a, the math lib
-  "-lnosys" # link against stub version of std/posix functions
-  # "LINKER:--end-group"
-  "-fno-common" # Default setting, specifies compiler placement for uninitialized global variables to be in BSS
-  "-ffunction-sections" # Place each function item into its own section in the output file, this helps the linker optimize code
-  "-fdata-sections" # Place each data item into its own section in the output file, this helps the linker optimize code
-  "-ffreestanding" # Asserts that the target is a freestanding environment, ie one which the standard lib may not exist
-  "-fno-builtin" # Do not recognize built in function that do not begin with __buildin_ as prefix
-  "-static" # prevents linking with shared libraries, only allow static linking
-  "-mapcs"
-  "LINKER:--print-memory-usage"
-  "LINKER:--gc-sections"
-  "LINKER:--allow-multiple-definition"
+#   "--specs=nano.specs"
+#   # "--specs=nosys.specs"
+#   # "LINKER:--start-group"
+#   "-lc" # link with libc.a, the standard c lib
+#   "-lm" # link with libm.a, the math lib
+#   "-lnosys" # link against stub version of std/posix functions
+#   # "LINKER:--end-group"
+#   "-fno-common" # Default setting, specifies compiler placement for uninitialized global variables to be in BSS
+#   "-ffunction-sections" # Place each function item into its own section in the output file, this helps the linker optimize code
+#   "-fdata-sections" # Place each data item into its own section in the output file, this helps the linker optimize code
+#   "-ffreestanding" # Asserts that the target is a freestanding environment, ie one which the standard lib may not exist
+#   "-fno-builtin" # Do not recognize built in function that do not begin with __buildin_ as prefix
+#   "-static" # prevents linking with shared libraries, only allow static linking
+#   "-mapcs"
+#   "LINKER:--print-memory-usage"
+#   "LINKER:--gc-sections"
+#   "LINKER:--allow-multiple-definition"
   "LINKER:--no-warn-rwx-segments" # Disable "has a LOAD segment with RWX permissions" warning, only in gcc 12 or greater
 )
 
-add_link_options(
-  "$<$<CONFIG:Debug>:-g>"
-)
+# add_link_options(
+#   "$<$<CONFIG:Debug>:-g>"
+# )
